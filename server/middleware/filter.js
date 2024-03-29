bf55269755109090c67c401378c887fa56ac7e74
Jessica Grant <jessicarobyn10@gmail.com>
2024-03-28 21:28:27 -0400

    Restore file

---
+++ server/middleware/filter.js
@@ -0,0 +1,97 @@
+module.exports = {
+  filterResponse: async (req, res, next) => {
+    try {
+      // grab API key from node env variables, deconstruct formId, and define filters
+      const apiKey = process.env.API_KEY;
+      const { formId } = req.params;
+      const filters = req.query.filters ? JSON.parse(req.query.filters) : [];
+      
+      // reformat filters into a reference object to avoid having to iterate through filters array (assumes unique IDs for all questions)
+      const filterObj = filters.reduce((acc, cv) => (
+        acc[cv.id] = {'value': cv.value, 'condition': cv.condition }, acc
+      ), {})
+
+      // return an error if the formId is not provided
+      if (!formId) {
+        return next({
+          log: 'Error in filterController.filterResponse, client did not provide a form Id',
+          message: 'Unable to get responses, no formId provided',
+        });
+      }
+
+      // fetch submissions
+      const formData = await fetch(
+        `https://api.fillout.com/v1/api/forms/${formId}/submissions`,
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
+      );
+
+      // parse submission data
+      const formDataParsed = await formData.json();
+
+      // store page count in a constant to use in response
+      const pageCount = formDataParsed.pageCount;
+
+      // filter submission data using requested filters
+      const filter = (submissions, filters) => {
+        if (!filters.length) return submissions;
+        const results = [];
+
+        submissions.forEach((submission) => {
+          const questions = submission.questions;
+          let passes = true;
+          questions.forEach((question) => {
+            if (filterObj[question.id]) {
+              const filter = filterObj[question.id];
+              if (
+                filter.condition === 'greater_than' &&
+                filter.value >= question.value
+              ) {
+                passes = false;
+              } else if (
+                filter.condition === 'less_than' &&
+                filter.value <= question.value
+              ) {
+                passes = false;
+              } else if (
+                filter.condition === 'equals' &&
+                filter.value !== question.value
+              ) {
+                passes = false;
+              } else if (
+                filter.condition === 'does_not_equal' &&
+                filter.value === question.value
+              ) {
+                passes = false;
+              }
+            }
+          });
+          if (passes) {
+            results.push(submission);
+          }
+        });
+        return results;
+      };
+      
+      const filteredData = filter(formDataParsed.responses, filters);
+
+      const response = {
+        reponses: filteredData,
+        totalResponses: filteredData.length,
+        pageCount: pageCount,
+      };
+
+      res.locals.data = response;
+      return next();
+
+    } catch (err) {
+      return next({
+        log: 'Error in filterController.filterResponse',
+        message: 'Error getting survey responses'
+      });
+    }
+  },
+};
---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const PORT = 3333;
+require('dotenv').config();
+
+app.use(express.json());
+
+const filterController = require('./middleware/filter');
+
+// HTTP get request for fetching form responses
+app.get('/:formId/filteredResponses', filterController.filterResponse, (req, res) => {
+  return res.status(200).json(res.locals.data);
+});
+
+// 404 handler for unknown routes
+app.use('*', (req, res) => {
+  return res.status(404).send('Page not found')
+})
+
+// global error handler
+app.use((err, req, res, next) => {
+  const DefaultErr = {
+    log: 'Express server caught unknown middleware error', 
+    status: 500,
+    message: { err: 'Server error' }
+  }
+  const ErrorObj = Object.assign(DefaultErr, err);
+  console.log(ErrorObj.log);
+  return res.status(ErrorObj.status).json(ErrorObj.message);
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+});
+
+module.exports = app;
